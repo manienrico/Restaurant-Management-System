f946c276bfc32b466afb4cba11abb44283973833
Enrico Mani <manienrico13@gmail.com>
2023-10-19 08:44:06 +0300

    user context and user provider structure

---
+++ src/context/user.context.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+//actual value you want to access
+export const UserContext = createContext({
+    
+})
+
+export const UserProvider =({ children })=>{
+    return <UserContext.Provider></UserContext.Provider>
+}
